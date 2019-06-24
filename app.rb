f5ffde068bd144d64d7ee275d424f6a8be55b310
TimeSmash <TimeSmash@github.com>
2019-06-24 11:26:44 -0400

    Added explanations of app.rb contents

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
+#The heart and soul of a Sinatra
+#Aka application controller
+
 require 'sinatra'
 class App < Sinatra::Base
 
+  #using Sinatra,
+  # method responds to get request from 
+  #root url and displays Hello World!
   get '/' do 
     "Hello, world!"
   end
